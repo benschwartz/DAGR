d8379735a4e516677bafe0ce3fd3778ac5347440
Ben Schwartz <benschwart@gmail.com>
2013-12-07 16:05:08 -0500

    Almost done: TODO: Check for file already in DAGR. Fix time fields

--- src/BulkDigester.java
+++ src/BulkDigester.java
@@ -1,14 +1,177 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.FileOwnerAttributeView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FilenameUtils;
 
 public class BulkDigester {
 
+	private static final Logger LOGGER = Logger.getLogger("DAGR");
+
+	private static final Options options;
+
+	private static final DirectoryStream.Filter<Path> directoryFilter = new DirectoryStream.Filter<Path>() {
+		@Override
+		public boolean accept(Path entry) throws IOException {
+			return Files.isDirectory(entry) && !Files.isSymbolicLink(entry);
+		}
+	};
+
+	private static final DirectoryStream.Filter<Path> fileFilter = new DirectoryStream.Filter<Path>() {
+		@Override
+		public boolean accept(Path entry) throws IOException {
+			return !Files.isDirectory(entry);
+		}
+	};
+
+	static {
+		options = new Options();
+		options.addOption("p", true, "path");
+		options.addOption("r", false, "recursively descend into directory");
+	}
+
 	public static void main(String[] args) {
-		if(Utils.connect()){
-			Utils.disconnnect();
+		CommandLineParser parser = new BasicParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+			if (!cmd.hasOption("p")) {
+				throw new ParseException("");
+			}
+		} catch (ParseException e) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("java -jar DAGR.jar", options);
+			return;
 		}
-		else{
-			throw new RuntimeException();
+		if (!Utils.connect()) {
+			return;
 		}
+		String parent = Utils.getFileSystemUUID();
+		processDirectory(parent, cmd.getOptionValue("p"), cmd.hasOption("r"));
+		Utils.disconnnect();
+	}
+
+	private static void processDirectory(String parent, String path,
+			boolean descend) {
+		String uuid = UUID.randomUUID().toString();
+		Path directoryPath = FileSystems.getDefault().getPath(path);
+		String DAGRName = directoryPath.getFileName().toString();
+		BasicFileAttributeView attribs = Files.getFileAttributeView(
+				directoryPath, BasicFileAttributeView.class,
+				LinkOption.NOFOLLOW_LINKS);
+		FileOwnerAttributeView ownerAttribs = Files.getFileAttributeView(
+				directoryPath, FileOwnerAttributeView.class,
+				LinkOption.NOFOLLOW_LINKS);
+		String DAGRType = "directory";
+		try {
+			long size = Files.size(directoryPath);
+			long createTime = attribs.readAttributes().creationTime()
+					.toMillis();
+
+			long modifiedTime = attribs.readAttributes().lastModifiedTime()
+					.toMillis();
+			String owner = ownerAttribs.getOwner().getName();
+			LOGGER.info("Processed directory: " + path + "\nname: " + DAGRName
+					+ "\nparent uuid: " + parent + "\nuuid: " + uuid + "\ntype: "
+					+ DAGRType + "\ncreate time: " + createTime + "\nmodified time: " + modifiedTime + "\nowner: "
+					+ owner + "\nsize: " + size);
+			Utils.insertDAGR(uuid, DAGRName, "FROM_UNIXTIME(" + createTime
+					+ ")","FROM_UNIXTIME(" + modifiedTime
+					+ ")", directoryPath.toString(), parent, owner,
+					DAGRType, size);
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "could not get attributes of " + path, e);
+			return;
+		}
+		List<String> files = getFiles(path);
+		for (String file : files) {
+			processFile(uuid, file);
+		}
+		if (descend) {
+			for (String subdir : getSubdirectories(path)) {
+				processDirectory(uuid, subdir, descend);
+			}
+		}
+
+	}
+
+	private static void processFile(String parent, String file) {
+		String uuid = UUID.randomUUID().toString();
+		Path filePath = FileSystems.getDefault().getPath(file);
+		BasicFileAttributeView attribs = Files.getFileAttributeView(filePath,
+				BasicFileAttributeView.class, LinkOption.NOFOLLOW_LINKS);
+		FileOwnerAttributeView ownerAttribs = Files.getFileAttributeView(
+				filePath, FileOwnerAttributeView.class,
+				LinkOption.NOFOLLOW_LINKS);
+		String DAGRName = filePath.getFileName().toString();
+		String DAGRType = FilenameUtils.getExtension(filePath.toString());
+		try {
+			long size = Files.size(filePath);
+			Long createTime = attribs.readAttributes().creationTime()
+					.toMillis();
+			Long modifiedTime = attribs.readAttributes().lastModifiedTime()
+					.toMillis();
+			String owner = ownerAttribs.getOwner().getName();
+			Utils.insertDAGR(uuid, DAGRName, "FROM_UNIXTIME(" + createTime
+					+ ")","FROM_UNIXTIME(" + modifiedTime
+					+ ")", filePath.toString(), parent, owner, DAGRType,
+					size);
+			LOGGER.info("Processed file: " + file + "\nname: " + DAGRName
+					+ "\nparent uuid: " + parent + "\nuuid: " + uuid + "\ntype: "
+					+ DAGRType + "\ncreate time: " + createTime + "\nmodified time: " + modifiedTime + "\nowner: "
+					+ owner + "\nsize: " + size);
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "could not get attributes of " + file, e);
+		}
+	}
+
+	private static List<String> getFiles(String path) {
+		List<String> files = new ArrayList<String>();
+		try {
+			Path dir = FileSystems.getDefault().getPath(path);
+			DirectoryStream<Path> stream = Files.newDirectoryStream(dir,
+					fileFilter);
+			for (Path p : stream) {
+				files.add(p.toAbsolutePath().toString());
+			}
+			stream.close();
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Could not get the files in " + path, e);
+		}
+		return files;
+	}
+
+	private static List<String> getSubdirectories(String path) {
+		List<String> subdirs = new ArrayList<String>();
+		try {
+			Path dir = FileSystems.getDefault().getPath(path);
+			DirectoryStream<Path> stream = Files.newDirectoryStream(dir,
+					directoryFilter);
+			for (Path p : stream) {
+				subdirs.add(p.toAbsolutePath().toString());
+			}
+			stream.close();
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Could not get subdirectories of " + path,
+					e);
+		}
+		return subdirs;
 	}
 
 }
--- src/Utils.java
+++ src/Utils.java
@@ -2,28 +2,31 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class Utils {
 	private static Logger LOGGER = Logger.getLogger("DAGR");
-	
+
 	private static Connection conn = null;
-	
-	static{
+
+	static {
 		Properties props = new Properties();
-		InputStream iStream = Utils.class.getResourceAsStream("creds.properties");
+		InputStream iStream = Utils.class
+				.getResourceAsStream("creds.properties");
 		try {
-			if(iStream==null){
-				throw new IOException("resource creds.properties does not exist on the classpath");
+			if (iStream == null) {
+				throw new IOException(
+						"resource creds.properties does not exist on the classpath");
 			}
 			props.load(iStream);
 			iStream.close();
 		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE,"Could not load properties", e);
+			LOGGER.log(Level.SEVERE, "Could not load properties", e);
 			throw new ExceptionInInitializerError(e);
 		}
 		URL = props.getProperty("URL");
@@ -33,25 +36,25 @@ public class Utils {
 	private static final String URL;
 	private static final String user;
 	private static final String password;
-	
-	public static boolean connect(){
-		if(conn!=null){
-			LOGGER.log(Level.WARNING,"connection already open");
+
+	public static boolean connect() {
+		if (conn != null) {
+			LOGGER.log(Level.WARNING, "connection already open");
 			return false;
 		}
-		try{
-			conn = DriverManager.getConnection("jdbc:mysql://" + URL + "?user=" + user + "&password="+password);
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://" + URL, user,
+					password);
 			return true;
-		}
-		catch(SQLException e){
+		} catch (SQLException e) {
 			LOGGER.log(Level.SEVERE, "could not get connection", e);
 			return false;
 		}
 	}
-	
-	public static void disconnnect(){
-		if(conn==null){
-			LOGGER.log(Level.WARNING,"close called on null connection");
+
+	public static void disconnnect() {
+		if (conn == null) {
+			LOGGER.log(Level.WARNING, "close called on null connection");
 			return;
 		}
 		try {
@@ -60,12 +63,86 @@ public class Utils {
 			LOGGER.log(Level.SEVERE, "could not close connection", e);
 		}
 	}
-	
-	public static void executeStatement(){
-		
+
+	public static void executeStatement() {
+
+	}
+
+	public static void executeQuery() {
+
+	}
+
+	public static void insertDAGR(String GUID, String name, String create_date, String modify_date,
+			String location, String parentGUID, String author, String type,
+			long size) {
+		GUID = encodeString(GUID);
+		name = encodeString(name);
+		location = encodeString(location);
+		parentGUID = encodeString(parentGUID);
+		author = encodeString(author);
+		type = encodeString(type);
+		try {
+			Statement stmt = conn.createStatement();
+			//AuthorID
+			int authorID = -1;
+			ResultSet rs = stmt
+					.executeQuery("Select author_id from author where name='"
+							+ author + "';");
+			if (rs.next()) {
+				authorID = rs.getInt("author_id");
+			} else {
+				rs.close();
+				String insert = "INSERT INTO author (name) VALUES ('" + author
+						+ "');";
+				LOGGER.info(insert);
+				stmt.execute(insert);
+				rs = stmt
+						.executeQuery("Select author_id from author where name='"
+								+ author + "';");
+				if (!rs.next()) {
+					rs.close();
+					stmt.close();
+					LOGGER.log(Level.SEVERE, "could not insert DAGR with GUID "
+							+ GUID);
+					return;
+				}
+				authorID = rs.getInt("author_id");
+				rs.close();
+			
+			}
+			// Type
+			rs = stmt.executeQuery("Select type from type where type = '" + type + "'");
+			if(!rs.next()){
+				stmt.execute("INSERT INTO type (type) VALUES ('" + type + "');");
+			}
+			rs.close();
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO dagr (GUID,name,date_created,date_modified,location,parent_GUID,author_id,type,size) ");
+			sql.append("VALUES (");
+			sql.append("'" + GUID + "',");
+			sql.append("'" + name + "',");
+			sql.append(create_date + ",");
+			sql.append(modify_date + ",");
+			sql.append("'" + location + "',");
+			sql.append("'" + parentGUID + "',");
+			sql.append(authorID + ",");
+			sql.append("'" + type + "',");
+			sql.append(size);
+			sql.append(");");
+			LOGGER.info(sql.toString());
+			stmt.execute(sql.toString());
+			stmt.close();
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, "Could not insert DAGR with GUID " + GUID,
+					e);
+		}
+	}
+
+	public static String getFileSystemUUID() {
+		return "FS_ROOT";
 	}
 	
-	public static void executeQuery(){
-		
+	private static String encodeString(String str){
+		return str.replace("\\", "\\\\").replace("'","\'").replace("\"", "\\\"");
 	}
 }
